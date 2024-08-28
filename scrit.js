4e18fe52fe4c0edf3b0975b06d480b7eca55c376
tanex-pain <animewallah910175@gmail.com>
2024-08-28 19:26:59 +0530

    Tanex

---
+++ scrit.js
@@ -0,0 +1,14 @@
+const audio = new Audio();
+audio.src = "/rolex.mp3"
+function myFunction() {
+    alert("more update coming soon");
+}
+var typed = new Typed(".auto-type",{
+    strings: ["Programmer", "Designer", "Student"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    looped: true
+    })
+    function myFunction() {
+        alert("More Update Coming Soon!");
+    }
